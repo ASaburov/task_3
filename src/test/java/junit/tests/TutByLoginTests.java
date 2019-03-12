431a3cb1c0ef3ebd2a0e8fc8d73bf285c633c3b0
Alexey Saburov <AlexeySaburov@coherentsolutions.com>
2019-03-12 23:24:48 +0300

    C3: By variables example added

--- src/test/java/junit/tests/TutByLoginTests.java
+++ src/test/java/junit/tests/TutByLoginTests.java
@@ -5,13 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.PageFactory;
 import pages.TutByHomePage;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class TutByLoginTests {
 
